3cb79662c2dd379885173d16c16d33160954f0a5
Tarek Shikdar <tarekshikdar09@gmail.com>
2025-07-06 12:20:54 +0600

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -1,4 +1,6 @@
-const { getStreamFromURL } = require("fb-watchman");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   config: {
@@ -14,20 +16,37 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, args, message, usersData }) {
-    const videoURL = "https://files.catbox.moe/ewf5cy.mp4";
-    const attachment = await global.utils.getStreamFromURL(videoURL);
-
     const id = event.senderID;
     const userData = await usersData.get(id);
     const name = userData.name;
     const mention = [{ id, tag: name }];
+
+    // 🛠 Convert Google Drive view link to direct download link
+    const fileId = "1QQ4rcb5mnLytHKuavPxOjx0rF-YuOTaS";
+    const directURL = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    // ⏬ Download the file temporarily
+    const filePath = path.join(__dirname, "owner-video.mp4");
+    const response = await axios({
+      url: directURL,
+      method: "GET",
+      responseType: "stream"
+    });
+
+    const writer = fs.createWriteStream(filePath);
+    response.data.pipe(writer);
+
+    await new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
 
     const info = 
 `━━━━━━━━━━━━━━━━
 👋 𝗛𝗲𝗹𝗹𝗼, ${name}
 
 📌 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢
-• 𝗡𝗮𝗺𝗲➝ᴇʀʀᴏʀ
+• 𝗡𝗮𝗺𝗲➝ ᴇʀʀᴏʀ
 • 𝗣𝗿𝗲𝗳𝗶𝘅 ➝ .
 
 👤 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢
@@ -35,14 +54,14 @@ module.exports = {
 • 𝗚𝗲𝗻𝗱𝗲𝗿 ➝ 𝗠𝗮𝗹𝗲
 • 𝗔𝗴𝗲 ➝ 18+
 • 𝗦𝘁𝗮𝘁𝘂𝘀 ➝ 𝗦𝗶𝗻𝗴𝗹𝗲
-• 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 ➝ 𝗗𝗶𝗽𝗹𝗼𝗺𝗮 𝗶𝗻 𝗰𝗶𝘃𝗶𝗹 𝗘𝗻𝗴𝗶𝗻𝗲𝗲𝗿𝗶𝗻𝗴
+• 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 ➝ 𝗗𝗶𝗽𝗹𝗼𝗺𝗮 𝗶𝗻 𝗖𝗶𝘃𝗶𝗹 𝗘𝗻𝗴𝗶𝗻𝗲𝗲𝗿𝗶𝗻𝗴
 • 𝗟𝗼𝗰𝗮𝘁𝗶𝗼𝗻 ➝ 𝗧𝗮𝗻𝗴𝗮𝗶𝗹
 ━━━━━━━━━━━━━━━━━`;
 
     message.reply({
       body: info,
       mentions: mention,
-      attachment
+      attachment: fs.createReadStream(filePath)
     });
   }
 };
