7cf79287b2564c858fa2f209555537aea1cb8549
Tarek Shikdar <tarekshikdar09@gmail.com>
2025-09-03 13:11:47 +0600

    Update and rename pair3.js to pair.js

--- scripts/cmds/pair.js
+++ scripts/cmds/pair.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports = {
   config: {
-    name: "pair3",
+    name: "pair",
     version: "mention-fix-ultra",
     author: "Tarek Shikdar",
     countDown: 5,
