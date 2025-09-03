fcc34235252da5185ecd1d8e5ca3ee4db8d41e2f
Tarek Shikdar <tarekshikdar09@gmail.com>
2025-09-03 13:11:18 +0600

    Update and rename pair2.js to ypair2.js

--- scripts/cmds/ypair2.js
+++ scripts/cmds/ypair2.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs-extra");
 module.exports = {
   config: {
-    name: "pair2",
+    name: "pair4",
     countDown: 10,
     role: 0,
     shortDescription: {
