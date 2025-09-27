2c2c7c72ed7a3cfcbc990244c23159165695be71
Tarek Shikdar <tarekshikdar09@gmail.com>
2025-09-27 11:19:00 +0600

    Update cmdLogger.js

--- scripts/events/cmdLogger.js
+++ scripts/events/cmdLogger.js
@@ -3,19 +3,18 @@ const { getTime } = global.utils;
 module.exports = {
   config: {
     name: "cmdLogger",
-    version: "1.0",
-    author: "Tarek",
-    category: "system",
-    isEvent: true
+    version: "1.1",
+    author: "TAREK",
+    category: "events"
   },
 
-  onChat: async ({ event, message, api, usersData, threadsData }) => {
+  onStart: async ({ event, api, usersData }) => {
     try {
       const { threadID, senderID, body } = event;
       const { config } = global.GoatBot;
       const logGroupID = config.logGroupID || "1879628072949507";
 
-      // যদি মেসেজ prefix দিয়ে শুরু হয়, তখনই লগ করবো
+      // যদি prefix না থাকে তাহলে return
       const prefix = config.prefix || "/";
       if (!body || !body.startsWith(prefix)) return;
 
