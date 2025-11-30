80352ea047b406cba8e3ecbec8bbc41cff5ba195
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-11-30 20:33:46 +0600

    Update gf.js

--- scripts/cmds/gf.js
+++ scripts/cmds/gf.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-	 name: "gf",
+	 name: "gfx",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "gf") {
  return message.reply({
- body: " 「 BESSAR BUKE\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃」",
+ body: " 「 BESSAR BUKE\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n✨AYAN✨」",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/nWD4xk3.mp4")
  });
  }
