276cc3b729565fb56b09f7598e4751cbbca27b5a
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-12-21 21:10:23 +0600

    Update pussy.js

--- scripts/cmds/pussy.js
+++ scripts/cmds/pussy.js
@@ -2,31 +2,63 @@ module.exports = {
 	config: {
 		name: "pussy",
 		aliases: ["18+"],
-		version: "1.0",
-		author: "Doru fix by kivv",
+		version: "1.1",
+		author: "Doru fix by kivv | Maya",
 		countDown: 5,
-		role: 2,
+		role: 1,
 		shortDescription: "send you pic of pussy",
-		longDescription: "sends u pic of girls pussy",
+		longDescription: "sends u pic of girls pussy (VIP only, paid)",
 		category: "18+",
 		guide: "{pn}"
 	},
 
-	onStart: async function ({ message }) {
-	 var link = [ 
-"https://i.ibb.co/jfqMF07/image.jpg",
-"https://i.ibb.co/tBBCS4y/image.jpg",
-"https://i.ibb.co/3zpyMVY/image.jpg",
-"https://i.ibb.co/gWbWT8k/image.jpg",
-"https://i.ibb.co/mHtyD1P/image.jpg",
-"https://i.ibb.co/vPHNhdY/image.jpg",
-"https://i.ibb.co/rm6rPjb/image.jpg",
-"https://i.ibb.co/7GpN2GW/image.jpg",
-"https://i.ibb.co/CnfMVpg/image.jpg",
-	]
-let img = link[Math.floor(Math.random()*link.length)]
-message.send({
-	body: '「 Pussy💦🥵 」',attachment: await global.utils.getStreamFromURL(img)
-})
-}
-		 }
+	onStart: async function ({ message, usersData, event }) {
+		const senderID = event.senderID;
+
+		// ===== GET USER DATA =====
+		const userData = await usersData.get(senderID);
+
+		// VIP CHECK
+		if (!userData || userData.vip !== true) {
+			return message.send(
+				"🔒 | **VIP ONLY**\n\n🥺 Baby, তুমি VIP না। আগে VIP নাও তারপর এই 18+ command use করো 💋"
+			);
+		}
+
+		// BALANCE CHECK (100M)
+		const cost = 100_000_000;
+		const balance = userData.money || 0;
+
+		if (balance < cost) {
+			return message.send(
+				`💸 | **Insufficient Balance**\n\nএই command use করতে 100M balance লাগবে 🥺\nতোমার balance: ${balance.toLocaleString()}`
+			);
+		}
+
+		// DEDUCT BALANCE
+		await usersData.set(senderID, {
+			money: balance - cost
+		});
+
+		// =========================
+
+		const links = [ 
+			"https://i.ibb.co/jfqMF07/image.jpg",
+			"https://i.ibb.co/tBBCS4y/image.jpg",
+			"https://i.ibb.co/3zpyMVY/image.jpg",
+			"https://i.ibb.co/gWbWT8k/image.jpg",
+			"https://i.ibb.co/mHtyD1P/image.jpg",
+			"https://i.ibb.co/vPHNhdY/image.jpg",
+			"https://i.ibb.co/rm6rPjb/image.jpg",
+			"https://i.ibb.co/7GpN2GW/image.jpg",
+			"https://i.ibb.co/CnfMVpg/image.jpg",
+		];
+
+		const img = links[Math.floor(Math.random() * links.length)];
+
+		message.send({
+			body: `「 Pussy💦🥵 」\n\n💰 100M balance deducted`,
+			attachment: await global.utils.getStreamFromURL(img)
+		});
+	}
+};
