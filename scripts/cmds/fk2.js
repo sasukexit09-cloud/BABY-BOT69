5fc6d4f02a7f3b4950b0ca64c535b458bff48319
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-12-24 12:29:34 +0600

    Update fk2.js

--- scripts/cmds/fk2.js
+++ scripts/cmds/fk2.js
@@ -7,8 +7,8 @@ module.exports = {
   config: {
     name: "fuck2",
     aliases: ["fk2"],
-    version: "1.0",
-    author: "Efat",
+    version: "1.1",
+    author: "Efat + Maya Fix",
     countDown: 5,
     role: 2,
     shortDescription: "FK with custom image",
@@ -17,77 +17,79 @@ module.exports = {
     guide: "{pn} @mention"
   },
 
+  fetchAvatar: async function(uid) {
+    try {
+      const url = `https://graph.facebook.com/${uid}/picture?width=512&height=512&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`;
+      const res = await axios.get(url, { responseType: "arraybuffer" });
+      return await Canvas.loadImage(res.data);
+    } catch {
+      const img = new Canvas.Canvas(512, 512);
+      const ctx = img.getContext("2d");
+      ctx.fillStyle = "#cccccc";
+      ctx.fillRect(0, 0, 512, 512);
+      return img;
+    }
+  },
+
   onStart: async function ({ api, message, event, usersData }) {
     const mention = Object.keys(event.mentions);
     if (mention.length === 0) return message.reply("Please mention someone to FK.");
 
-    let senderID = event.senderID;
-    let mentionedID = mention[0];
+    const senderID = event.senderID;
+    const mentionedID = mention[0];
 
     try {
-      // Detect gender (fallback to male/female)
       const senderData = await usersData.get(senderID);
       const mentionedData = await usersData.get(mentionedID);
 
       const senderGender = senderData.gender || "male";
       const mentionedGender = mentionedData.gender || "female";
 
-      let maleID, femaleID;
+      let maleID = senderGender === "male" ? senderID : mentionedID;
+      let femaleID = senderGender === "female" ? senderID : mentionedID;
 
-      if (senderGender === "male") {
-        maleID = senderID;
-        femaleID = mentionedID;
-      } else {
-        maleID = mentionedID;
-        femaleID = senderID;
-      }
-
-      // Fetch avatars
-      const avatarMale = await usersData.getAvatarUrl(maleID);
-      const avatarFemale = await usersData.getAvatarUrl(femaleID);
-
-      const [avatarImgMale, avatarImgFemale] = await Promise.all([
-        Canvas.loadImage(avatarMale),
-        Canvas.loadImage(avatarFemale)
+      const [avatarMale, avatarFemale] = await Promise.all([
+        this.fetchAvatar(maleID),
+        this.fetchAvatar(femaleID)
       ]);
 
-      // Load background image
-      const bgUrl = "https://files.catbox.moe/0d5987.mp4";
+      // Background image
+      const bgUrl = "https://i.imgur.com/PlVBaM1.jpg"; // static image link
       const bgRes = await axios.get(bgUrl, { responseType: "arraybuffer" });
       const bg = await Canvas.loadImage(bgRes.data);
 
-      // Canvas setup
+      // Canvas
       const canvasWidth = 900;
       const canvasHeight = 600;
       const canvas = Canvas.createCanvas(canvasWidth, canvasHeight);
       const ctx = canvas.getContext("2d");
-
-      // Draw background
       ctx.drawImage(bg, 0, 0, canvasWidth, canvasHeight);
 
-      // Avatar settings
+      // Avatar positions
       const avatarSize = 230;
-      const y = canvasHeight / 2 - avatarSize - 90;
+      const yPos = canvasHeight / 2 - avatarSize / 2;
 
-      // Draw female avatar (left)
+      // Female avatar (left)
       ctx.save();
+      const femaleX = 150;
       ctx.beginPath();
-      ctx.arc(100 + avatarSize / 2, y + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2);
+      ctx.arc(femaleX + avatarSize / 2, yPos + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2);
       ctx.closePath();
       ctx.clip();
-      ctx.drawImage(avatarImgFemale, 100, y, avatarSize, avatarSize);
+      ctx.drawImage(avatarFemale, femaleX, yPos, avatarSize, avatarSize);
       ctx.restore();
 
-      // Draw male avatar (right)
+      // Male avatar (right)
       ctx.save();
+      const maleX = canvasWidth - 150 - avatarSize;
       ctx.beginPath();
-      ctx.arc(canvasWidth - 150 - avatarSize / 2, y + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2);
+      ctx.arc(maleX + avatarSize / 2, yPos + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2);
       ctx.closePath();
       ctx.clip();
-      ctx.drawImage(avatarImgMale, canvasWidth - 150 - avatarSize, y, avatarSize, avatarSize);
+      ctx.drawImage(avatarMale, maleX, yPos, avatarSize, avatarSize);
       ctx.restore();
 
-      // Save and send image
+      // Save & send
       const imgPath = path.join(__dirname, "tmp", `${maleID}_${femaleID}_fk.png`);
       await fs.ensureDir(path.dirname(imgPath));
       fs.writeFileSync(imgPath, canvas.toBuffer("image/png"));
@@ -99,7 +101,7 @@ module.exports = {
 
     } catch (err) {
       console.error("Error in fk command:", err);
-      message.reply("There was an error creating the fk image.");
+      message.reply("There was an error creating the FK image.");
     }
   }
 };
