eb4b817b86301e901e6fdbe2c3956c326d607334
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2026-01-18 12:31:43 +0600

    Update boobies.js

--- scripts/cmds/boobies.js
+++ scripts/cmds/boobies.js
@@ -1,44 +1,66 @@
-const request = require("request");
-const fs = require("fs");
 const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
-module.exports.config = {
-  name: "boobies",
-  version: "1.0.0",
-  hasPermssion: 2,
-  credits: "Kaneki",
-  description: "Squeeze the breast of the tagged user",
-  commandCategory: "18+ Command",
-  usages: "[tag]",
-  cooldowns: 5,
-};
+module.exports = {
+  config: {
+    name: "boobies",
+    version: "1.1.0",
+    author: "Kaneki & Gemini",
+    countDown: 5,
+    role: 0, // সবার জন্য উন্মুক্ত রাখতে ০, এডমিন হলে ২ দিন
+    shortDescription: { en: "Squeeze the breast of the tagged user (Anime GIF)" },
+    category: "nsfw",
+    guide: { en: "{pn} @mention or reply" }
+  },
 
-module.exports.run = async({ api, event, Threads, global }) => {
-  var links = [
-    "https://i.postimg.cc/tC2BTrmF/3.gif",
-    "https://i.postimg.cc/pLrqnDg4/78d07b6be53bea612b6891724c1a23660102a7c4.gif",
-    "https://i.postimg.cc/gJFD51nb/detail.gif",
-    "https://i.postimg.cc/xjPRxxQB/GiC86RK.gif",
-    "https://i.postimg.cc/L8J3smPM/tumblr-myzq44-Hv7-G1rat3p6o1-500.gif",
-  ];
+  onStart: async function ({ api, event }) {
+    const { threadID, messageID, mentions, messageReply, senderID } = event;
+    let targetID, name;
 
-  var mention = Object.keys(event.mentions);
-  let tag = event.mentions[mention].replace("@", "");
+    // ১. টার্গেট আইডি এবং নাম নির্ধারণ
+    if (messageReply) {
+      targetID = messageReply.senderID;
+      name = "প্রিয়"; // রিপ্লাইতে নাম ডিটেক্ট করা জটিল তাই ডিফল্ট
+    } else if (Object.keys(mentions).length > 0) {
+      targetID = Object.keys(mentions)[0];
+      name = mentions[targetID].replace("@", "");
+    } else {
+      return api.sendMessage("⚠️ দয়া করে একজনকে মেনশন করুন অথবা মেসেজে রিপ্লাই দিন!", threadID, messageID);
+    }
 
-  if (!mention) return api.sendMessage("Please tag 1 person", event.threadID, event.messageID);
+    const links = [
+      "https://i.postimg.cc/tC2BTrmF/3.gif",
+      "https://i.postimg.cc/pLrqnDg4/78d07b6be53bea612b6891724c1a23660102a7c4.gif",
+      "https://i.postimg.cc/gJFD51nb/detail.gif",
+      "https://i.postimg.cc/xjPRxxQB/GiC86RK.gif",
+      "https://i.postimg.cc/L8J3smPM/tumblr-myzq44-Hv7-G1rat3p6o1-500.gif"
+    ];
 
-  var callback = () => 
-    api.sendMessage(
-      {
-        body: `${tag} 𝗬𝗼𝘂 𝗚𝗲𝘁 𝗬𝗼𝘂𝗿 𝗕𝗿𝗲𝗮𝘀𝘁 𝗦𝗾𝘂𝗲𝗲𝘇𝗲𝗱 😝`,
-        mentions: [{ tag: tag, id: Object.keys(event.mentions)[0] }],
-        attachment: fs.createReadStream(__dirname + "/cache/bopvu.gif")
-      }, 
-      event.threadID, 
-      () => fs.unlinkSync(__dirname + "/cache/bopvu.gif")
-    );
+    const cacheDir = path.join(process.cwd(), "cache");
+    if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir, { recursive: true });
+    const gifPath = path.join(cacheDir, `squeeze_${Date.now()}.gif`);
 
-  return request(encodeURI(links[Math.floor(Math.random() * links.length)]))
-    .pipe(fs.createWriteStream(__dirname + "/cache/bopvu.gif"))
-    .on("close", () => callback());
+    try {
+      const randomLink = links[Math.floor(Math.random() * links.length)];
+      
+      // ২. ইমেজ/গিফ ডাউনলোড
+      const response = await axios.get(randomLink, { responseType: "arraybuffer" });
+      fs.writeFileSync(gifPath, Buffer.from(response.data));
+
+      // ৩. মেসেজ পাঠানো
+      return api.sendMessage({
+        body: `${name} 𝗬𝗼𝘂 𝗚𝗲𝘁 𝗬𝗼𝘂𝗿 𝗕𝗿𝗲𝗮𝘀𝘁 𝗦𝗾𝘂𝗲𝗲𝘇𝗲𝗱 😝`,
+        mentions: [{ tag: name, id: targetID }],
+        attachment: fs.createReadStream(gifPath)
+      }, threadID, () => {
+        if (fs.existsSync(gifPath)) fs.unlinkSync(gifPath);
+      }, messageID);
+
+    } catch (err) {
+      console.error(err);
+      if (fs.existsSync(gifPath)) fs.unlinkSync(gifPath);
+      return api.sendMessage("❌ গিফ লোড করতে সমস্যা হয়েছে!", threadID, messageID);
+    }
+  }
 };
